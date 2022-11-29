8ad0fbd008a16321c62005228c7eabb2ed5a1ad9
Manzar <manzar.hussain@telus.com>
2022-11-28 22:49:06 -0500

    Final commit for Template Engine - EJS (TinyApp Assignment)

--- express_server.js
+++ express_server.js
@@ -16,7 +16,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
@@ -40,11 +40,8 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
+});
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
