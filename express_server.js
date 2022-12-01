fbccadef0854fdf411a8165f95cb54a1a7865510
Manzar <manzar.hussain@telus.com>
2022-11-30 20:10:45 -0500

    Cookies in Express assignment completed

--- express_server.js
+++ express_server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
+
+app.use(cookieParser());
+app.use(morgan('dev'));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -42,8 +48,6 @@ app.post('/urls/:id', (req, res) => {
   res.redirect(`/urls`);
 });
 
-
-
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const shortURL = generateRandomString();
@@ -56,19 +60,37 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+// Following three get methods update for Cookies in Expres Assignment
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};//username cookie
+      res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, 
+  longURL: urlDatabase[req.params.id],
+  username: req.cookies["username"]}; // username cookie
   res.render("urls_show", templateVars);
 });
+
+// added 2 post methods for "login" and "logout"
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  const username = req.body.username;
+  res.clearCookie('username', username);
+  res.redirect('/urls');
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
